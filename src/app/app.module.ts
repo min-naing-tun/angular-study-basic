03e353abc7a46ee8bccb1d5f0f103d1cd7d40c58
Min Naing Tun <minnaingtun.pro@gmail.com>
2023-08-18 16:30:06 +0630

    routing_and_router_links_work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'form',
+        component: FormComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
