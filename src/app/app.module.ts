01dcde3b388288bb73ee7e4f0f001481528a8611
minnaingtun.pro@gmail.com <minnaingtun.pro@gmail.com>
2023-08-20 22:52:11 +0630

    DataBindingLearning_String-Interpolation_Property_Event_Two-Way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -19,6 +19,7 @@ import { AppRouting } from './sysgen/AppRouting';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRouting
   ],
   providers: [],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+  myAddress: string = "267/8, YanAung 5 St, Yankin Township";
+  myPhone: number = 9252660486;
+  myEmail: string = "minnaingtun@mmeasenet.com"
+  myFlag: boolean = false;
+  userMessage: string = ""
+
+
+  clickFunction() {
+    this.myFlag = !this.myFlag;
+    alert("Address : "+this.myAddress+"\nPhone : "+this.myPhone+"\nEmail : "+this.myEmail+"\nUser Message => "+this.userMessage);
+  }
+
+  resetFunction(){
+    this.userMessage = "";
+    this.myFlag = false;
+  }
 
 }
