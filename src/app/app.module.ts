20e7fcb1ecf181bb1c9107b14ebab715ad835e59
Min Naing Tun <minnaingtun.pro@gmail.com>
2023-08-23 13:29:08 +0630

    Template-Driven-Form_and_Modal-Driven-Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -29,6 +30,7 @@ import {SortStringArrayPipe} from './about/SortStringArrayPipe'
     UserFormShowComponent
   ],
   imports: [
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserModule,
     FormsModule,
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  skillData: string[] = ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'Jquery', 'Angular', 'NodeJs'];
+  myForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      rFirstName : new FormControl(),
+      rLastName: new FormControl(),
+      rEmail: new FormControl(),
+      rPassword: new FormControl(),
+      rSkill: new FormControl()
+    })
+  }
+
+  submitData(userForm: string[]){
+    console.log(userForm);
+  }
 
 }
