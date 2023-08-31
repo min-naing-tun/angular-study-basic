0b7a5f6e1776ca3bfa559cc39ef7b13023eac25e
Min Naing Tun <minnaingtun.pro@gmail.com>
2023-08-31 17:12:05 +0630

    Sending-And-Getting-Route-Parameter-Values

--- src/app/user-form-show/comD.component.ts
+++ src/app/user-form-show/comD.component.ts
@@ -1,11 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
+
 
 @Component({
     selector: 'my-comD',
     template: `
     <h1>This is component D</h1>
+    <p>
+        User is click {{userClickId}} button
+    </p>
     `,
     styles: [``]
 })
 
-export class ComD{}
+export class ComD implements OnInit{
+    userClickId: number = 0;
+    ngOnInit(): void {
+        this.userClickId = this.myActivateRoute.snapshot.params["id"]; // getting route parameter
+    }
+
+    constructor(private myActivateRoute: ActivatedRoute) {
+        
+    }
+}
--- src/app/user-form-show/comHome.component.ts
+++ src/app/user-form-show/comHome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'my-ComHome',
@@ -9,12 +10,30 @@ import { Component } from '@angular/core';
     <a routerLink="ComC" class="p-3">comC</a>&nbsp;&nbsp;&nbsp;
     <a [routerLink]="['ComD', 10]" class="p-3">comD</a> <!--Route with route parameters-->
     </div>
+    <br>
+
+    <!--Click event with route and it's parameter values-->
+    <button type="button" (click)="idButtonClicked(1)" class="btn btn-primary">One</button>&nbsp;&nbsp;&nbsp;
+    <button type="button" (click)="idButtonClicked(2)" class="btn btn-primary">Two</button>&nbsp;&nbsp;&nbsp;
+    <button type="button" (click)="idButtonClicked(3)" class="btn btn-primary">Three</button>&nbsp;&nbsp;&nbsp;
+    <button type="button" (click)="idButtonClicked(4)" class="btn btn-primary">Four</button>&nbsp;&nbsp;&nbsp;
+    <button type="button" (click)="idButtonClicked(5)" class="btn btn-primary">Five</button>&nbsp;&nbsp;&nbsp;
+
 
     <br>
     <router-outlet></router-outlet>
+    <br>
+    
     `
 })
 
 export class ComHome {
 
+    constructor(private myRouter: Router) {
+
+    }
+
+    idButtonClicked(userClickId: number){
+        this.myRouter.navigate(['userFormShow/ComD', userClickId]); //After clicked and send route data to another component
+    }
 }
