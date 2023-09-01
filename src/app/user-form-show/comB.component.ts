bb467acc368d1bcb4e428aba3d846ef667b47dc8
Min Naing Tun <minnaingtun.pro@gmail.com>
2023-09-01 10:58:21 +0630

    Sending-Fragment-Parameter

--- src/app/user-form-show/comB.component.ts
+++ src/app/user-form-show/comB.component.ts
@@ -4,8 +4,22 @@ import {Component} from '@angular/core';
     selector: 'my-comB',
     template: `
     <h1>This is component two</h1>
+    <hr>
+    <div id="sectionOne" style="background-color: lightblue; width: 100%; height: 1000px; color: white; font-size: 50px; text-align: center;">
+        Section 1
+    </div>
+    <div id="sectionTwo" style="background-color: grey; width: 100%; height: 1000px; color: white; font-size: 50px; text-align: center;">
+        Section 2
+    </div>
+    <div id="sectionThree" style="background-color: black; width: 100%; height: 1000px; color: white; font-size: 50px; text-align: center;">
+        Section 3
+    </div>
+    <div id="sectionFour" style="background-color: green; width: 100%; height: 1000px; color: white; font-size: 50px; text-align: center;">
+        Section 4
+    </div>
     `,
-    styles: [``]
+    styles: [`
+    `]
 })
 
 export class ComB{
--- src/app/user-form-show/comHome.component.ts
+++ src/app/user-form-show/comHome.component.ts
@@ -18,8 +18,13 @@ import { Router } from '@angular/router';
     <button type="button" (click)="idButtonClicked(3)" class="btn btn-primary">Three</button>&nbsp;&nbsp;&nbsp;
     <button type="button" (click)="idButtonClicked(4)" class="btn btn-primary">Four</button>&nbsp;&nbsp;&nbsp;
     <button type="button" (click)="idButtonClicked(5)" class="btn btn-primary">Five</button>&nbsp;&nbsp;&nbsp;
+    <br><br>
 
-
+    <!--Click event with route and sending fragment-->
+    <button type="button" [routerLink]="['ComB']" fragment="sectionOne" class="btn btn-secondary">Go To Section One</button>&nbsp;&nbsp;&nbsp;
+    <button type="button" [routerLink]="['ComB']" fragment="sectionTwo" class="btn btn-secondary">Go To Section Two</button>&nbsp;&nbsp;&nbsp;
+    <button type="button" [routerLink]="['ComB']" fragment="sectionThree" class="btn btn-secondary">Go To Section Three</button>&nbsp;&nbsp;&nbsp;
+    <button type="button" (click)="fragmentSending()" class="btn btn-secondary">Go To Section Four</button>&nbsp;&nbsp;&nbsp;
     <br>
     <router-outlet></router-outlet>
     <br>
@@ -35,5 +40,9 @@ export class ComHome {
 
     idButtonClicked(userClickId: number){
         this.myRouter.navigate(['userFormShow/ComD', userClickId]); //After clicked and send route data to another component
+    }
+
+    fragmentSending(){
+        this.myRouter.navigate(['userFormShow/ComB'], {fragment: "sectionFour"}); //sending fragment
     }
 }
